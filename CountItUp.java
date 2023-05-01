a6817a98ac6868f12a68d88ee0a08eb1e6770e5a
Hamzah Alansi <alaha971@student.otago.ac.nz>
2023-05-02 09:43:02 +1200

    Implemented multiplication + converted value to string

--- CountItUp.java
+++ CountItUp.java
@@ -18,10 +18,6 @@ public class CountItUp {
         int k;
         int[] arrN = new int[SIZE];
         int[] arrK = new int[SIZE];
-        Arrays.fill(arrN, 0);
-        Arrays.fill(arrK, 0);
-        arrN[0] = 1;
-        arrK[0] = 1;
         // System.out.println(Long.MAX_VALUE);
         if (args.length > 0) {
             try {
@@ -40,18 +36,28 @@ public class CountItUp {
             n = Integer.parseInt(parts[0]);
             k = Integer.parseInt(parts[1]);
 
+            Arrays.fill(arrN, 0);
+            arrN[0] = 1;
             // First for loop cycle to calculate factorial of n
             for (int i = 1; i <= n; i++) {
                 factorial(arrN, i); // arr = arr * i. Initially arr was 1.
             }
-            print(arrN);
+            System.out.println(Long.MAX_VALUE);
+            System.out.println("-------");
+            // print(arrN);
 
+            Arrays.fill(arrK, 0);
+            arrK[0] = 1;
             // Second for loop cycle to calculate factorial of k
             for (int i = 1; i <= k; i++) {
                 factorial(arrK, i); // arr = arr * i. Initially arr was 1.
             }
-            print(arrK);
+            System.out.println(toStringValue(arrN));
+            System.out.println(toStringValue(arrK));
 
+            System.out.println("-----------");
+
+            System.out.println(multiply(toStringValue(arrK), toStringValue(arrN)));
         }
         scanner.close();
     }
@@ -82,21 +88,48 @@ public class CountItUp {
         }
     }
 
-    // This module prints the number stored in arr. Maximum size can be SIZE.
-    public static void print(int[] arr) {
-        int flag = 0;
+    public static String multiply(String num1, String num2) {
+        String a = num1;
+        String b = num2;
+        if (a.equals("0") || b.equals("0")) {
+            System.out.println("0");
+            return "0";
+        }
+        int m = a.length() - 1, n = b.length() - 1,
+                carry = 0;
+        String product = "";
+        for (int i = 0; i <= m + n || carry != 0; ++i) {
+            for (int j = Math.max(0, i - n); j <= Math.min(i, m); ++j) {
+                carry += (a.charAt(m - j) - '0')
+                        * (b.charAt(n - i + j) - '0');
+            }
+            product += (char) (carry % 10 + '0');
+            carry /= 10;
+        }
+        product = new StringBuilder(product)
+                .reverse()
+                .toString();
+        // System.out.println("The Product is: " + product);
+        return product;
+    }
 
+    // This module prints the number stored in arr. Maximum size can be SIZE.
+    public static String toStringValue(int[] arr) {
+        int flag = 0;
+        String val = "";
         for (int i = SIZE - 1; i >= 0; i--) {
             if (arr[i] != 0) {
                 flag = 1;
             }
 
             if (flag == 1) {
-                System.out.print(arr[i]);
+                // System.out.print(arr[i]);
+                val += arr[i];
             }
         }
 
-        System.out.println();
+        // System.out.println();
+        return val;
     }
 
     // factorial method takes a long parameter n, and returns the factorial of that
