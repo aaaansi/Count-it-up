c3cf4b87d8392b3da1d6a89e4ff6a3ad1d4c5cee
Hamzah Alansi <alaha971@student.otago.ac.nz>
2023-05-09 19:59:06 +1200

    Added continuous scanning

--- CountItUpFalse.java
+++ CountItUpFalse.java
@@ -1,3 +1,5 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -5,14 +7,28 @@ import java.util.Scanner;
 public class CountItUpFalse {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] input = sc.nextLine().split(" ");
-        long n = Long.parseLong(input[0]);
-        long k = Long.parseLong(input[1]);
+        long result = 0;
+        if (args.length > 0) {
+            try {
+                sc = new Scanner(new File(args[0]));
+            } catch (FileNotFoundException e) {
+                System.err.println("File not found: " + args[0]);
+                System.exit(1);
+            }
+
+        } else { // otherwise, use standard input
+            sc = new Scanner(System.in);
+        }
+        while (sc.hasNextLine()) {
+            String[] input = sc.nextLine().split(" ");
+            long n = Long.parseLong(input[0]);
+            long k = Long.parseLong(input[1]);
+
+            result = binomial(n, k);
+            System.out.println(result);
+        }
+
         sc.close();
-
-        long result = binomial(n, k);
-
-        System.out.println(result);
     }
 
     private static long binomial(long n, long k) {
